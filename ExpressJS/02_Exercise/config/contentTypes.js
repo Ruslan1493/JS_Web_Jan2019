b544507ff72a6feb69e2863463e955b32fe3dc08
Ruslan1493 <star93@list.ru>
2019-05-16 17:10:13 +0300

    Removed some files

--- ExpressJS/02_Exercise/config/contentTypes.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  'css': 'text/css',
-  'jpg': 'image/jpeg',
-  'js': 'text/javascript',
-  'png': 'image/png',
-  'ico': 'image/x-icon'
-}
--- ExpressJS/04_Exercise/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    development: {
-        port: '3000',
-        dbPath: 'mongodb://admin:abc123@ds211865.mlab.com:11865/expressjs_exercise04'
-    },
-    production: {}
-};
